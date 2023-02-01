b6703663811df1e81ae260b4c1201e8b0ba57ab9
Jangkeun Lim <ygeat7@gmail.com>
2023-02-01 17:33:18 +0900

    페이지 구조 및 url 수정

--- machine_data/urls.py
+++ machine_data/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.search_page),
     path('<str:pk>/', views.machine_detail_page),
-    path('', views.index)
+    path('searchresult/', views.index), #검색결과 관련 함수 필요...   
 ]
--- production_django_prj/urls.py
+++ production_django_prj/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('searchresult/', include('machine_data.urls')),
+    path('machinedata/', include('machine_data.urls')),
     path("admin/", admin.site.urls),
 ]
