413c7a01568a77fc59b91ebe61708f78a58ee302
Jangkeun Lim <ygeat7@gmail.com>
2023-02-01 17:33:18 +0900

    페이지 구조 및 url 수정

--- machine_data/views.py
+++ machine_data/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import MachineData
 
+def search_page(request):
+    return render(
+        request,
+        'machine_data/search_page.html',
+    )
+
+# 검색기능 구현 필요
 def index(request):
     records = MachineData.objects.all()
     return render(
         request,
-        'machine_data/result.html',
+        'machine_data/search_result.html',
         {
             'records':records
         }
