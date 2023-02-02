8e770508b16a2501a760c58ffc43f10173c93a43
Jangkeun Lim <ygeat7@gmail.com>
2023-02-03 08:30:49 +0900

    검색 기능 구현 중

--- machine_data/urls.py
+++ machine_data/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.search_page),
     path('<str:pk>/', views.machine_detail_page),
-    path('searchresult/', views.index), #검색결과 관련 함수 필요...   
+    path('searchresult/', views.MachineList.as_view()),
+    path('searchresult/<str:q>/', views.MachineSearch.as_view()),
+    #path('searchresult/', views.index), #검색결과 관련 함수 필요...
 ]
--- machine_data/views.py
+++ machine_data/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 from .models import MachineData
+from django.views.generic import ListView
+
+from django.db.models import Q
 
 def search_page(request):
     return render(
         request,
         'machine_data/search_page.html',
-    )
-
-# 검색기능 구현 필요
-def index(request):
-    records = MachineData.objects.all()
-    return render(
-        request,
-        'machine_data/search_result.html',
-        {
-            'records':records
-        }
     )
 
 def machine_detail_page(request, pk):
@@ -28,3 +20,40 @@ def machine_detail_page(request, pk):
             'record':record,
         }
     )
+
+# 검색기능 구현 필요
+#def index(request):
+#    records = MachineData.objects.all()
+#    return render(
+#        request,
+#        'machine_data/search_result.html',
+#        {
+#            'records':records
+#        }
+#    )
+class MachineList(ListView):
+    model = MachineData
+    template_name = 'machine_data/search_result.html'
+
+    paginate_by = 5
+
+class MachineSearch(MachineList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        machine_list = MachineData.objects.filter(
+            Q(new_l_processsettingname__contains=q) | Q(new_l_machinehistoryname__contains=q)
+        ).distinct()
+        return machine_list
+
+    def get_context_data(self, **kwargs):
+        context = super(MachineSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
+
+
+
+
